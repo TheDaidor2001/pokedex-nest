95f82d852184415734820e95c1df8404f4d3b476
TheDaidor2001 <danielcastillo1552001@hotmail.com>
2024-02-01 12:36:45 +0100

    empty

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function main() {
@@ -6,6 +7,12 @@ async function main() {
 
   app.setGlobalPrefix('api/v2'); //Añadir la api/${rutas}
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+   );
 
   await app.listen(3000);
 }
